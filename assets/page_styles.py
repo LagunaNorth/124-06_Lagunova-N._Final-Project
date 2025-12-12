9ea809e5e103da4e4ba1ad9211330b7962d86e89
Nadezhda Lagunova <lagunovana@gmail.com>
2025-12-12 20:59:19 +0300

    Update page_styles.py

--- assets/page_styles.py
+++ assets/page_styles.py
@@ -3,7 +3,7 @@ GRAPH_TITLE_ALIGN = 0.5
 GRAPH_FONT_WEIGHT = 500
 GRAPH_FONT_FAMILY = 'Rubik'
 GRAPH_FONT_COLOR = '#5b55c1'
-GRAPH_FONT_SIZE = 20
+GRAPH_FONT_SIZE = 19
 GRAPH_TITLE_FONT_COLOR = '#f97025'
 
 PLOT_BACKGROUND_COLOR = 'rgba(220, 227, 231, 0.5)'
@@ -15,4 +15,5 @@ DROPDOWN_STYLE = {'color': '#5b55c1',
                   'border': '1px solid rgba(249,112,37, 0.5)',
                   'borderRadius': '5px',
                   }
+
 
